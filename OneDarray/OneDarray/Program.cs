4fd818d4e1676fb8784428add08a9161bb5ff12d
Raj Gaurav <49223793+raj99gaurav@users.noreply.github.com>
2022-01-24 09:30:17 +0530

    Add files via upload

---
+++ OneDarray/OneDarray/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneDarray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // declares a 1D Array of string.
+            string[] weekDays;
+
+            // allocating memory for days.
+            weekDays = new string[] {"Sun", "Mon", "Tue", "Wed",
+                                       "Thu", "Fri", "Sat"};
+
+            // Displaying Elements of array
+            foreach (string day in weekDays)
+                Console.Write(day + " ");
+            Console.ReadLine();
+        }
+    }
+}
